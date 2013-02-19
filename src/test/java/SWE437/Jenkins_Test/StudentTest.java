46bdaf71bb1934e571bff8b0115195efd3ab6fd5
Chaoran Lu <clu3@gmu.edu>
2013-02-19 17:35:00 -0500

    Chaoran broke the build
    Oops, wrong name

--- src/test/java/SWE437/Jenkins_Test/StudentTest.java
+++ src/test/java/SWE437/Jenkins_Test/StudentTest.java
@@ -35,7 +35,7 @@ public class StudentTest {
 	@Test
 	public void studentNameGetter(){
 		s = new Student("James", 12);
-		assertEquals("James", s.getName());
+		assertEquals("Jackie", s.getName());
 	}
 
 	@Test
