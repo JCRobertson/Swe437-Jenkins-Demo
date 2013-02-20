c226f3e0bf4abc3a8c0bd73201f6bcfd71643a15
Kevin Lu <klu3@gmu.edu>
2013-02-20 00:37:27 -0500

    Fixed the build

--- src/test/java/SWE437/Jenkins_Test/StudentTest.java
+++ src/test/java/SWE437/Jenkins_Test/StudentTest.java
@@ -35,7 +35,7 @@ public class StudentTest {
 	@Test
 	public void studentNameGetter(){
 		s = new Student("James", 12);
-		assertEquals("Kevin", s.getName());
+		assertEquals("James", s.getName());
 	}
 
 	@Test
